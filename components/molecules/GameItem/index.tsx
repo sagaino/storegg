4d86bcc0864c5fc94d0e6a583ec6116b3dd60d59
sagaino <sagaino18@gmail.com>
2022-03-14 16:22:25 +0700

    fix img GameItem in mobile

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -22,6 +22,7 @@ export default function GameItem(props: GameItemProps) {
               width={205}
               height={270}
               alt="thumbnail"
+              layout="fixed"
             />
           </div>
           <div className="cover position-absolute bottom-0 m-32">
@@ -32,6 +33,7 @@ export default function GameItem(props: GameItemProps) {
                   width={54}
                   height={36}
                   alt="console"
+                  layout="fixed"
                 />
               </div>
               <div>
